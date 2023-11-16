d84246896abcebfc51aa2569da22fe19ce4b6bb7
Roberto <roberto.v.arebalo@gmail.com>
2023-11-16 08:10:29 -0300

    Se agrega parte de la logica del ejercicio, funciona mal

---
+++ js/ejercicio2.js
@@ -0,0 +1,67 @@
+/*
+2-Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
+Una propiedad titular con el valor "Alex".
+Una propiedad saldo, teniendo como valor inicial 0.
+Un método ingresar() que permita añadir dinero a la cuenta, pasando la cantidad como parámetro
+Un método extraer() que permita retirar la cantidad pasada como parámetro.
+Un método informar() que retorne la información del estado de la cuenta. 
+
+Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y 
+volver a mostrar la descripción del estado de la cuenta.
+
+Rectángulos
+*/
+
+class cuenta {
+    #titular ;
+    #saldo ;
+
+    constructor(titular,saldo,){
+        this.#titular = titular;
+        this.#saldo = saldo;
+    }
+    get titular(){
+        return this.#titular
+    }
+
+    set ingresar(monto){
+        if (monto > 0) {
+            this.#saldo = this.#saldo + monto;
+        }
+        else{
+            document.write(`El importe tiene que ser mayor a 0 <br>`);
+        }
+        this.informar();
+    }
+    set extraer(monto){
+        if (this.#saldo -monto > 0) {
+            this.#saldo = this.#saldo - monto;
+        } else {
+            document.write(`No tiene el saldo suficiente para hacer la extraccion. <br>`);
+        }
+        this.informar();
+
+    }
+
+    get informar(){
+        document.write(`Su saldo actual es ${this.#saldo}<br>`)
+    }
+
+}
+
+const nuevaCuenta = new cuenta("Alex",0);
+
+document.write(`Consulto el nombre de la cuenta:  ${nuevaCuenta.titular} <br>`);
+//document.write(`Consulto el saldo de la cuenta:  ${nuevaCuenta.informar} <br>`);
+//ingreso 16 pesos
+nuevaCuenta.ingresar = -16;
+
+nuevaCuenta.ingresar = 19;
+
+nuevaCuenta.informar;
+
+nuevaCuenta.extraer = 10 ;
+
+nuevaCuenta.informar
+
+
