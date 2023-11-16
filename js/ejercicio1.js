01edb668c11167d14593828fbf332dd183b61bc7
Roberto <roberto.v.arebalo@gmail.com>
2023-11-15 21:19:41 -0300

    Agrego el ejercicio1 terminado

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -13,8 +13,23 @@ El auto se apagó
 
 const auto = {
     color: 'rojo',
-    marca: 'Ferrari',
+    marca: 'ferrari',
     modelo: '74',
     encendido: false,
-
+    
+    arrancar: function(){
+        this.encendido = !this.encendido;
+        if (this.encendido) {
+            document.write("Tu nave acaba de despertar y en 5 minutos (después de calentar) estará lista para devorar el asfalto. <br>");
+        }
+        else{
+            document.write("Tu nave se ha ido a descansar. <br>");
+        }
+    }
 }
+
+document.write(`${auto.encendido}<br>`);
+
+auto.arrancar();
+
+document.write(auto.encendido);
