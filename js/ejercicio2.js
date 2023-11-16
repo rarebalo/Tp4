332b5799b0366b8cf576a3bc7d1055395fa215a9
rarebalo <94885185+rarebalo@users.noreply.github.com>
2023-11-16 23:30:35 +0000

    agrego el ejercicio 2 terminado

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -13,55 +13,54 @@ Rectángulos
 */
 
 class cuenta {
-    #titular ;
-    #saldo ;
+    #titular;
+    #saldo;
 
-    constructor(titular,saldo,){
+    constructor(titular, saldo) {
         this.#titular = titular;
         this.#saldo = saldo;
     }
-    get titular(){
-        return this.#titular
+
+    get titular() {
+        return this.#titular;
     }
 
-    set ingresar(monto){
+    ingresar(monto) {
         if (monto > 0) {
             this.#saldo = this.#saldo + monto;
-        }
-        else{
+        } else {
             document.write(`El importe tiene que ser mayor a 0 <br>`);
         }
         this.informar();
     }
-    set extraer(monto){
-        if (this.#saldo -monto > 0) {
+
+    extraer(monto) {
+        if (this.#saldo - monto > 0) {
             this.#saldo = this.#saldo - monto;
         } else {
             document.write(`No tiene el saldo suficiente para hacer la extraccion. <br>`);
         }
         this.informar();
-
     }
 
-    get informar(){
-        document.write(`Su saldo actual es ${this.#saldo}<br>`)
+    informar() {
+        document.write(`Su saldo actual es ${this.#saldo}<br>`);
     }
-
 }
 
-const nuevaCuenta = new cuenta("Alex",0);
+const nuevaCuenta = new cuenta("Alex", 0);
 
 document.write(`Consulto el nombre de la cuenta:  ${nuevaCuenta.titular} <br>`);
 //document.write(`Consulto el saldo de la cuenta:  ${nuevaCuenta.informar} <br>`);
 //ingreso 16 pesos
-nuevaCuenta.ingresar = -16;
+nuevaCuenta.ingresar(16);
 
-nuevaCuenta.ingresar = 19;
+nuevaCuenta.informar();
 
-nuevaCuenta.informar;
+nuevaCuenta.extraer(10);
 
-nuevaCuenta.extraer = 10 ;
+nuevaCuenta.informar();
 
-nuevaCuenta.informar
+nuevaCuenta.extraer(9);
 
 
