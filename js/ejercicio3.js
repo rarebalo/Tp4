17282e74b8b8f168ee93a324ebba5c31471c10d8
rarebalo <94885185+rarebalo@users.noreply.github.com>
2023-11-17 00:50:11 +0000

    falta terminar, verificar por queno funciona

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -6,29 +6,52 @@ modificar y mostrar sus propiedades, calcular el perímetro y el área
 class rectangulo {
     alto;
     ancho;
-
-    constructor(alto, ancho){
-        this.alto = alto;
+  
+    constructor(alto, ancho) {
+      this.alto = alto;
+      this.ancho = ancho;
+    }
+  
+    get alto() {
+      return this.alto;
+    }
+  
+    get ancho() {
+      return this.ancho;
+    }
+  
+    get perimetro() {
+      let perimetro = (this.alto + this.ancho) * 2;
+      return perimetro;
+    }
+  
+    get area() {
+      let area = (this.alto * this.ancho) / 2;
+      return area;
+    }
+  
+    set alto(altura) {
+      if (altura > 0) {
+        this.alto = altura;
+      } else {
+        document.write("La altura debe ser mayor a 0.<br>");
+      }
+    }
+  
+    set ancho(ancho) {
+      if (ancho > 0) {
         this.ancho = ancho;
+      } else {
+        document.write("El ancho debe ser mayor a 0.<br>");
+      }
     }
-
-    get alto (){
-        return this.alto;
-    }
-    get ancho(){
-        return this.ancho;
-    }
-    get perimetro(){
-        let perimetro = (this.alto + this.ancho) * 2;
-        return perimetro;
-    }
-    get area(){
-        let area = (this.alto * this.ancho) / 2;
-        return area;
-    }
-}
-
-let nuevoRectangulo = new rectangulo(10, 5);
-
-document.write(`mues`)
-
+  }
+  
+  let nuevoRectangulo = new rectangulo(10, 5);
+  
+  document.write(`muestro el nuevo rectangulo: ${nuevoRectangulo.perimetro}<br>`);
+  nuevoRectangulo.alto(3);
+  nuevoRectangulo.ancho(7);
+  nuevoRectangulo.alto(0);
+  document.write(`muestro el nuevo rectangulo: ${nuevoRectangulo.perimetro}<br>`);
+  
