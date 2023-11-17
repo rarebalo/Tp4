598c746edbdfa6a0a6d3483d82792fdbb37c6b25
Roberto <roberto.v.arebalo@gmail.com>
2023-11-17 07:52:15 -0300

    Corrijo el ejercicio 3, faltan verificaciones

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -3,55 +3,57 @@ con las propiedades de alto y ancho, mas los métodos necesarios para
 modificar y mostrar sus propiedades, calcular el perímetro y el área
 */
 
-class rectangulo {
-    alto;
-    ancho;
-  
-    constructor(alto, ancho) {
-      this.alto = alto;
-      this.ancho = ancho;
-    }
-  
-    get alto() {
-      return this.alto;
-    }
-  
-    get ancho() {
-      return this.ancho;
-    }
-  
-    get perimetro() {
-      let perimetro = (this.alto + this.ancho) * 2;
-      return perimetro;
-    }
-  
-    get area() {
-      let area = (this.alto * this.ancho) / 2;
-      return area;
-    }
-  
-    set alto(altura) {
-      if (altura > 0) {
-        this.alto = altura;
-      } else {
-        document.write("La altura debe ser mayor a 0.<br>");
-      }
-    }
-  
-    set ancho(ancho) {
-      if (ancho > 0) {
-        this.ancho = ancho;
-      } else {
-        document.write("El ancho debe ser mayor a 0.<br>");
-      }
+class Rectangulo {
+  constructor(alto, ancho) {
+    this._alto = alto;
+    this._ancho = ancho;
+  }
+
+  get alto() {
+    return this._alto;
+  }
+
+  get ancho() {
+    return this._ancho;
+  }
+
+  get perimetro() {
+    const perimetro = (this._alto + this._ancho) * 2;
+    return perimetro;
+  }
+
+  get area() {
+    const area = this._alto * this._ancho;
+    return area;
+  }
+
+  set alto(altura) {
+    if (altura > 0) {
+      this._alto = altura;
+    } else {
+      document.write("La altura debe ser mayor a 0.<br>");
     }
   }
-  
-  let nuevoRectangulo = new rectangulo(10, 5);
-  
-  document.write(`muestro el nuevo rectangulo: ${nuevoRectangulo.perimetro}<br>`);
-  nuevoRectangulo.alto(3);
-  nuevoRectangulo.ancho(7);
-  nuevoRectangulo.alto(0);
-  document.write(`muestro el nuevo rectangulo: ${nuevoRectangulo.perimetro}<br>`);
+
+  set ancho(ancho) {
+    if (ancho > 0) {
+      this._ancho = ancho;
+    } else {
+      document.write("El ancho debe ser mayor a 0.<br>");
+    }
+  }
+}
+
+function escr(loQueEscribo){
+  document.write(`${loQueEscribo}<br>`)
+}
+
+let nuevoRectangulo = new Rectangulo(10, 5);
+
+document.write(`Perímetro del rectángulo: ${nuevoRectangulo.perimetro}<br>`);
+nuevoRectangulo.alto = 3;
+nuevoRectangulo.ancho = 7;
+nuevoRectangulo.alto = 0;
+escr(nuevoRectangulo._alto);
+escr(nuevoRectangulo._ancho);
   
