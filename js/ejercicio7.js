a51585d893b8921342024e55753957ec37e85509
Roberto <roberto.v.arebalo@gmail.com>
2023-11-19 23:10:26 -0300

    Agrego parte del ejercicio pero aun no funciona como corresponde

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -21,3 +21,86 @@ huecosLibres(): indica cuántos contactos más podemos ingresar.
 Crea un menú con opciones que serán seleccionadas por el usuario usando un prompt,
  las salidas de las operaciones seleccionadas por el usuario se pueden mostrar en pantalla y  por consola.
 */
+class Agenda {
+  constructor() {
+    this._cantidadDeContactos = 10;
+    this.contactos = [];
+  }
+
+  set anadirContacto(contacto) {
+    this.contactos.push(contacto);
+  }
+  set cantidadDeContactos(cantidad) {
+    this._cantidadDeContactos = cantidad;
+  }
+  get cantidadDeContactos() {
+    let cantidadDeContactos = this.contactos.filter(
+      (contacto) => contacto !== undefined
+    ).length;
+    return cantidadDeContactos;
+  }
+
+  existeContacto(consulta) {
+    let repetido = false;
+    const estadoConsulta = [];
+    for (let index = 0; index < this.contactos.length; index++) {
+      if (this.contactos[index][0] == consulta) {
+        estadoConsulta.push(
+          `El contacto "${consulta}" ya se encuentra agendado en la posicion n° ${
+            index + 1
+          }.`
+        );
+        estadoConsulta.push(index);
+        repetido = true;
+        break;
+      }
+    }
+    if (repetido) {
+      return estadoConsulta[0];
+    } else {
+      estadoConsulta.push(`El contacto "${consulta}" no existe en su agenda.`);
+      return estadoConsulta[0];
+    }
+  }
+
+  listarContactos() {
+    let listaDeContactos = "";
+    for (let index = 0; index < this.contactos.length; index++) {
+      listaDeContactos =
+        listaDeContactos +
+        this.contactos[index][0] +
+        " " +
+        this.contactos[index][1] +
+        "\n";
+    }
+    alert(listaDeContactos);
+  }
+  buscarContacto(nombre) {
+    let mensaje;
+    if (this.existeContacto(nombre).length > 0) {
+      mensaje = this.contactos[this.existeContacto(nombre)[1]];
+    } else {
+      mensaje = `El contacto "${nombre}" que buscas no existe en tu agenda.`  
+    }
+    return mensaje;
+  }
+}
+
+function unNumeroDeTelefono() {
+  const min = 1000000000;
+  const max = 9999999999;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let agendaRoberto = new Agenda();
+
+agendaRoberto.anadirContacto = ["Roberto Vicente", unNumeroDeTelefono()];
+agendaRoberto.anadirContacto = ["Carlos Leonardo", unNumeroDeTelefono()];
+agendaRoberto.anadirContacto = ["Florencia Ines", unNumeroDeTelefono()];
+
+alert(agendaRoberto.cantidadDeContactos);
+agendaRoberto.listarContactos();
+alert(agendaRoberto.existeContacto("Roberto Vicente"));
+alert(agendaRoberto.existeContacto("Juanito"));
+alert(agendaRoberto.buscarContacto("Juanito"));
+alert(agendaRoberto.buscarContacto("Carlos Leonardo"));
