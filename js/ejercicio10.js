fa309df05394572991857525b2364343171fefbe
Roberto <roberto.v.arebalo@gmail.com>
2023-11-20 23:30:54 -0300

    creo la estructura para comenzar a trabajar en el ejercicio10

---
+++ js/ejercicio10.js
@@ -0,0 +1,12 @@
+/*10- crear una clase aeropuerto con las propiedades nombreAeropuerto y lista de aviones,
+ esta clase deberá contener el método agregarAvion, el cual recibirá un objeto de tipo Avión,
+  además de un método buscarAvion el cual recibirá el nombre de un avión y devolverá información 
+  en caso de encontrarlo, si no lo encontró indicar con un mensaje.
+Por su parte los aviones tendrán las propiedades: nombre, capacidad, destino,
+ lista de pasajeros. Los aviones tienen el método abordar el cual permite que un
+  pasajero suba al avión solo si hay capacidad disponible en el mismo, caso contrario 
+  mostrar un mensaje que indique que el avión está lleno.
+
+Crear un objeto de tipo aeropuerto llamado "Aeropuerto Internacional", 
+crear 3 objetos aviones con diferentes destinos. Agregar los 3 aviones al aeropuerto, 
+buscar un avión y usar el método abordar.*/
