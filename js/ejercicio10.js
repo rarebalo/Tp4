ac200b9da712fb314b1450f2e9fa0b4164cc800a
Roberto <roberto.v.arebalo@gmail.com>
2023-11-21 07:43:17 -0300

    Agrego la clase aeropuerto

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -10,3 +10,46 @@ Por su parte los aviones tendrán las propiedades: nombre, capacidad, destino,
 Crear un objeto de tipo aeropuerto llamado "Aeropuerto Internacional", 
 crear 3 objetos aviones con diferentes destinos. Agregar los 3 aviones al aeropuerto, 
 buscar un avión y usar el método abordar.*/
+
+
+class Aeropuerto {
+  constructor(nombreAeropuerto, listaAviones){
+    this._nombreAeropuerto = nombreAeropuerto;
+    this._listaAviones = listaAviones;
+  }
+  set nombreAeropuerto(nombre){
+    this._nombreAeropuerto = nombre;
+  }
+  get nombreAeropuerto(){
+    return this._nombreAeropuerto;
+  }
+
+  set listaAviones(lista){
+    this._listaAviones = lista;
+  }
+
+  get listaAviones(){
+    return this._listaAviones;
+  }
+
+  agregarAvion(avion){
+    this._listaAviones.push(avion);
+  }
+
+  buscarAvion(nombre){
+    let encontrado = false;
+    for (let index = 0; index < this._listaAviones.length; index++) {
+      if (this._listaAviones[index].nombre === nombre) {
+        encontrado = true;
+        //infomracion de avion encontrado
+        break;
+      }      
+    }
+    if (encontrado) {
+      //informacion del avion encontrado
+    } else {
+      return let = "Avion no encontrado";
+    }
+  }
+}
+
